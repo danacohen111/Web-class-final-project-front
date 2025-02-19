06e408f65f9ca01d461222f1102bec762939bc20
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-02-19 17:24:07 +0200

    configure api

--- front/src/services/api-client.ts
+++ front/src/services/api-client.ts
@@ -1,9 +1,57 @@
 import axios from "axios";
+import type { CanceledError, AxiosRequestConfig, AxiosError, AxiosResponse } from "axios";
+export { CanceledError };
 
-
-const backend_url = import.meta.env.VITE_BACKEND_URL
 const apiClient = axios.create({
-    baseURL: backend_url,
+    baseURL: "http://localhost:3000",
 });
+
+apiClient.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+        const token = localStorage.getItem('accessToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error: AxiosError) => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response: AxiosResponse) => {
+        return response;
+    },
+    async (error: AxiosError) => {
+        const originalRequest = error.config;
+        if ((error.response?.status === 401 || error.response?.status === 403) && !originalRequest._retry) {
+            originalRequest._retry = true;
+            const refreshToken = localStorage.getItem('refreshToken');
+            if (refreshToken) {
+                try {
+                    const response = await axios.post('http://localhost:3060/auth/refresh', { refreshToken });
+                    const { accessToken, refreshToken: newRefreshToken,_id } = response.data;
+                    localStorage.setItem('accessToken', accessToken);
+                    localStorage.setItem('refreshToken', newRefreshToken);
+                    localStorage.setItem('userId',_id );
+
+                    originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                    return apiClient(originalRequest);
+                } catch (err) {
+                    console.error('Refresh token expired or invalid', err);
+                    localStorage.removeItem('accessToken');
+                    localStorage.removeItem('refreshToken');
+                    localStorage.removeItem('userId');
+
+                    window.location.href = '/login'; // Redirect to login page
+                }
+            } else {
+                window.location.href = '/login'; // Redirect to login page
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
