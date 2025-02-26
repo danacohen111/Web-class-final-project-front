3059d1cb3270e270a1acb665b38fb9c6abb92869
danacohen111 <danac2121@gmail.com>
2025-02-26 21:00:52 +0200

    add ai recommendations

--- front/src/config.ts
+++ front/src/config.ts
@@ -1,1 +1,3 @@
-export const OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY_ENV;
+export const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+export const OPENAI_API_PROJECT = import.meta.env.VITE_OPENAI_API_PROJECT;
+export const OPENAI_API_ORGANIZATION = import.meta.env.VITE_OPENAI_API_ORGANIZATION;
