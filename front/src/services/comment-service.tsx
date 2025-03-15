043690bc8479fd59e26cc523fd139c1e236d30c8
Ilana Belokon <ilana.belokon2@cs.colman.ac.il>
2025-03-15 17:50:59 +0200

    fix api call

--- front/src/services/comment-service.tsx
+++ front/src/services/comment-service.tsx
@@ -9,16 +9,10 @@ const CommentService = {
 
   async addComment(postId: string, userId: string, content: string): Promise<IComment>
    {
-    const accessToken = localStorage.getItem("accessToken");
-
     const response = await apiClient.post<IComment>("/comments", {
       post: postId,
       user: userId,
       content,
-    }, { headers: {
-          "Authorization": `JWT ${accessToken}`,
-          "Content-Type": "application/json"
-        }
     });
     return response.data;
   },
