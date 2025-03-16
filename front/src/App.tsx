70fa912cba70403927309684d9ef492ff8cec739
danacohen111 <danac2121@gmail.com>
2025-03-16 21:12:17 +0200

    fix routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from "react-router-dom";
-import Login from "./components/login/loginForm";
-import Profile from "./components/profile/profile";
-import AIRecommendations from "./components/recommendations/AiRecommendations";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import AppMenu from "./components/menu/appMenu";
-import SignupForm from "./components/signup/SignupForm";
-import Posts from './components/posts/RealEstatePost';
-import AddPostPage from "./components/posts/AddPost";
+import AppRoutes from "./components/routes/AppRoutes";
 
 const App: React.FC = () => {
   const location = useLocation();
@@ -14,15 +9,7 @@ const App: React.FC = () => {
   return (
     <div>
       {location.pathname !== "/login" && location.pathname !== "/signup" && <AppMenu />}
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/ai-recommendations" element={<AIRecommendations />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/addPost" element={<AddPostPage />} />
-      </Routes>
+      <AppRoutes />
     </div>
   );
 };
