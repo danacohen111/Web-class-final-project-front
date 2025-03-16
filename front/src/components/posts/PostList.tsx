a6e3f17e895477d294ffb3bd8de6e168ec92a519
danacohen111 <danac2121@gmail.com>
2025-03-16 19:59:39 +0200

    add paging

--- front/src/components/posts/PostList.tsx
+++ front/src/components/posts/PostList.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import Pagination from "../Pagination/Pagination";
 import { IPost } from "../../models/models";
 import "../../styles/postList.css";
+import "../../styles/Pagination.css";
 
 interface PostListProps {
   fetchPosts: () => Promise<IPost[]>;
@@ -10,6 +12,8 @@ interface PostListProps {
 const PostList: React.FC<PostListProps> = ({ fetchPosts }) => {
   const [posts, setPosts] = useState<IPost[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 3;
 
   useEffect(() => {
     if (!fetchPosts) return;
@@ -22,17 +26,24 @@ const PostList: React.FC<PostListProps> = ({ fetchPosts }) => {
       });
   }, [fetchPosts]);
 
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const startIndex = (currentPage - 1) * postsPerPage;
+  const selectedPosts = posts.slice(startIndex, startIndex + postsPerPage);
+
   return (
-    <div className="post-list">
-      {error && <p className="error">{error}</p>}
-      {posts.length > 0 ? (
-        posts.map((post) => ( 
-          <Post key={post._id} post={post} />
-        ))
-      ) : (
-        <p>No posts available.</p>
+    <>
+      <div className="post-list">
+        {error && <p className="error">{error}</p>}
+        {selectedPosts.length > 0 ? (
+          selectedPosts.map((post) => <Post key={post._id} post={post} />)
+        ) : (
+          <p>No posts available.</p>
+        )}
+      </div>
+      {totalPages > 1 && (
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
       )}
-    </div>
+    </>
   );
 };
 
